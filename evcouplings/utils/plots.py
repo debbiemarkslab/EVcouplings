f236a73d6ec314aa390b4e4ce632b2e76a969cab
Thomas Hopf <thomas.hopf@gmail.com>
2016-07-21 16:30:34 -0400

    Added: file for plots class

---
+++ evcouplings/utils/plots.py
@@ -0,0 +1,11 @@
+"""
+General plotting functions.
+
+Plotting specific to individual applications
+(e.g. mutation matrices) belong into respective submodule.
+
+Authors:
+  Thomas A. Hopf
+"""
+
+# TODO: Drop all the useful stuff from plotting_tools here...
