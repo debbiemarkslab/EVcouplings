d4f528b360dda7167837f7d442d304e5bcae2f4c
Thomas Hopf <thomas.hopf@gmail.com>
2016-07-24 20:22:14 -0400

    Added: File and notes for alignment app

---
+++ evcouplings/align/app.py
@@ -0,0 +1,25 @@
+"""
+Standalone alignment calculation tool
+
+# TODO: add logging here
+# TODO: add exception handling here
+
+Authors:
+  Thomas A. Hopf
+"""
+
+
+def run():
+    """
+    Run buildali after parsing config and command line arguments
+
+    # TODO: parameters this takes: config files...
+    # and command-line overrides for all of
+    # the most important parameters that change frequently
+
+    # TODO: how to name output files (prefix)?
+    # should this somehow contain the config file name?
+
+    # TODO: how to get alignment statistics and plots?
+    """
+    return
