ea25e8473753dbacb33ebe4f527ac19941a8b683
Thomas Hopf <thomas.hopf@gmail.com>
2016-07-26 19:29:37 -0400

    Added: function to run hhfilter

--- evcouplings/align/tools.py
+++ evcouplings/align/tools.py
@@ -140,3 +140,64 @@ def run_jackhmmer(query, database, prefix,
         )
 
     return result
+
+
+def run_hhfilter(input_file, output_file, threshold=95,
+                 columns="first", binary="hhfilter"):
+    """
+    Redundancy-reduce a sequence alignment using hhfilter
+    from the HHsuite alignment suite.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to input alignment in A2M/FASTA format
+    output_file : str
+        Path to output alignment (will be in A3M format)
+    threshold : int, optional (default: 95)
+        Sequence identity threshold for maximum pairwise
+        identity (between 0 and 100)
+    columns : {"first", "a2m"}
+        Definition of match columns (based on first sequence
+        or upper-case columns (a2m))
+    binary : str
+        Path to hhfilter binary
+
+    Returns
+    -------
+    str
+        output_file
+
+    Raises
+    ------
+    ResourceError
+        If output alignment is non-existent/empty
+    ValueError
+        Upon invalid value of columns parameter
+    """
+    if columns not in ["first", "a2m"]:
+        raise ValueError(
+            "Invalid column selection: {}".format(columns)
+        )
+
+    create_prefix_folders(output_file)
+
+    cmd = [
+        binary,
+        "-i", input_file,
+        "-o", output_file,
+        "-id", str(threshold),
+        "-M", columns,
+        "-v", str(2)
+    ]
+
+    return_code, stdout, stderr = run(cmd)
+
+    if not file_not_empty(output_file):
+        raise ResourceError(
+            "hhfilter returned empty alignment: stdout={} stderr={} file={}".format(
+                stdout, stderr, output_file
+            )
+        )
+
+    return output_file
