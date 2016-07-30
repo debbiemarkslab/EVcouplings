310f56269a86119cb313a1c88f91ddc3b58705d6
Thomas Hopf <thomas.hopf@gmail.com>
2016-07-30 16:33:30 -0400

    Fetch sequence and cut sequence fully implemented

--- evcouplings/align/protocol.py
+++ evcouplings/align/protocol.py
@@ -6,6 +6,7 @@ Authors:
 """
 
 import evcouplings.align.tools as at
+from evcouplings.align.alignment import read_fasta, write_fasta
 from evcouplings.utils.config import check_required
 from evcouplings.utils.system import (
     create_prefix_folders, get, file_not_empty, ResourceError
@@ -18,45 +19,127 @@ PROTOCOLS = {
 }
 
 
-def fetch_sequence(**kwargs):
+def fetch_sequence(sequence_id, sequence_file,
+                   sequence_download_url, out_file):
     """
     Get sequence.
 
     Parameters
     ----------
+    sequence_id : str
+        Identifier of sequence that should be retrieved
+    sequence_file : str
+        File containing sequence. If None, sqeuence will
+        be downloaded from sequence_download_url
+    sequence_download_url : str
+        URL from which to download missing sequence. Must
+        contain "{}" at the position where sequence ID will
+        be inserted into download URL (using str.format).
+    out_file : str
+        Output file in which sequence will be stored, if
+        sequence_file is not existing.
 
     Returns
     -------
-    str:
-        Path of file with sequence
+    str
+        Path of file with stored sequence (can be sequence_file
+        or out_file)
+    tuple (str, str)
+        Identifier of sequence as stored in file, and sequence
     """
-    check_required(
-        kwargs,
-        ["prefix", "sequence_id", "sequence_file"]
-    )
-
-    if kwargs["sequence_file"] is None:
-        # Only check sequence download URL if we actually
-        # need to download something
-        check_required(kwargs, ["sequence_download_url"])
-
-        outfile = kwargs["prefix"] + ".fa"
+    if sequence_file is None:
         get(
-            kwargs["sequence_download_url"].format(kwargs["sequence_id"]),
-            outfile,
+            sequence_download_url.format(sequence_id),
+            out_file,
             allow_redirects=True
         )
     else:
         # if we have sequence file, pass it through
-        outfile = kwargs["sequence_file"]
+        out_file = sequence_file
 
     # also make sure input file has something in it
-    if not file_not_empty(outfile):
+    if not file_not_empty(out_file):
         raise ResourceError(
-            "Input sequence missing: {}".format(outfile)
+            "Input sequence missing: {}".format(out_file)
         )
 
-    return outfile
+    with open(out_file) as f:
+        seq = next(read_fasta(f))
+
+    return out_file, seq
+
+
+def cut_sequence(sequence, sequence_id, region=None, first_index=None, out_file=None):
+    """
+    Cut a given sequence to sub-range and save it in a file
+
+    Parameters
+    ----------
+    sequence : str
+        Full sequence that will be cut
+    sequence_id : str
+        Identifier of sequence, used to construct header
+        in output file
+    region : tuple(int, int), optional (default: None)
+        Region that will be cut out of full sequence.
+        If None, full sequence will be returned.
+    first_index : int, optional (default: None)
+        Define index of first position in sequence.
+        Will be set to 1 if None.
+    out_file : str, optional (default: None)
+        Save sequence in a FASTA file (header:
+        >sequence_id/start_region-end_region)
+
+    Returns
+    ------
+    str
+        Subsequence contained in region
+    tuple(int, int)
+        Region. If no input region is given, this will be
+        (1, len(sequence)); otherwise, the input region is
+        returned.
+
+    Raises
+    ------
+    ValueError
+        Upon invalid region specification (violating boundaries
+        of sequence)
+    """
+    cut_seq = None
+
+    # (not using 1 as default value to allow parameter
+    # to be unspecified in config file)
+    if first_index is None:
+        first_index = 1
+
+    # last index is *inclusive*!
+    if region is None:
+        region = (first_index, first_index + len(sequence) - 1)
+        cut_seq = sequence
+    else:
+        start, end = region
+        str_start = start - first_index
+        str_end = end - first_index + 1
+        cut_seq = sequence[str_start:str_end]
+
+        # make sure bounds are valid given the sequence that we have
+        if str_start < 0 or str_end > len(sequence):
+            raise ValueError(
+                "Illegal sequence range: "
+                "region={} first_index={} len(sequence)={}".format(
+                    region,
+                    first_index,
+                    len(sequence)
+                )
+            )
+
+    # save sequence to file
+    if out_file is not None:
+        with open(out_file, "w") as f:
+            header = "{}/{}-{}".format(sequence_id, *region)
+            write_fasta([(header, cut_seq)], f)
+
+    return region, cut_seq
 
 
 def search_jackhmmer(**kwargs):
@@ -106,10 +189,15 @@ def standard(**kwargs):
 
     Returns
     -------
+
     """
     check_required(
         kwargs,
-        ["prefix", "sequence_id", "sequence_file"]
+        [
+            "prefix", "sequence_id", "sequence_file",
+            "sequence_download_url", "reuse_alignment",
+            "region", "first_index"
+        ]
     )
 
     prefix = kwargs["prefix"]
@@ -121,14 +209,36 @@ def standard(**kwargs):
         "statistics_file": prefix + "_alignment_statistics.csv"
     }
 
-    # make sure search sequence is defined
-    outcfg["sequence_file"] = fetch_sequence(**kwargs)
+    # make sure search sequence is defined and load it
+    outcfg["sequence_file"], (full_seq_id, full_seq) = fetch_sequence(
+        kwargs["sequence_id"], kwargs["sequence_file"],
+        kwargs["sequence_download_url"],
+        kwargs["prefix"] + ".fa"
+    )
 
+    # cut sequence to target region
+    # TODO: store this in output directory as region
+    region, cut_seq = cut_sequence(
+        full_seq,
+        kwargs["sequence_id"],
+        kwargs["region"],
+        kwargs["first_index"],
+        kwargs["prefix"] + "_searchseq.fa"
+    )
+
+    print(cut_seq)
     return outcfg
 
-    # run jackhmmer... allow to plonk in pre-exisiting sto file here
+    # run jackhmmer... allow to plonk in pre-exisiting sto/a2m file here
+    # make sure bitscores are expanded correctly
+    if kwargs["reuse_alignment"] is None:
+        # TODO: update outcfg?
+        # ali = search_jackhmmer(kwargs)
+        pass
+    else:
+        pass
 
-    # parse to a2m
+    # parse to a2m... autodetect which format we are starting from
 
     # apply id filter, gap threshold
 
