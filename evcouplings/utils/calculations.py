3b6e904b77631ed67dc02ba68ceb31271c1ee110
Thomas Hopf <thomas.hopf@gmail.com>
2016-11-25 20:28:55 -0500

    Added: entropy dictionary

--- evcouplings/utils/calculations.py
+++ evcouplings/utils/calculations.py
@@ -32,3 +32,32 @@ def entropy(X, normalize=False):
         return 1 - (H / np.log(len(X)))
     else:
         return H
+
+
+def entropy_map(model, normalize=True):
+    """
+    Parameters
+    ----------
+    model : CouplingsModel
+        Model for which entropy of sequence
+        alignment will be computed (based on
+        single-site frequencies f_i(A_i)
+        contained in model)
+    normalize : bool, default: True
+        Normalize entropy to range 0 (variable)
+        to 1 (conserved) instead of raw values
+
+    Returns
+    -------
+    dict
+        Map from positions in sequence (int) to
+        entropy of column (float) in alignment
+    """
+    cons = np.apply_along_axis(
+        lambda x: entropy(x, normalize=normalize),
+        axis=1, arr=model.fi()
+    )
+
+    return dict(
+        zip(model.index_list, cons)
+    )
