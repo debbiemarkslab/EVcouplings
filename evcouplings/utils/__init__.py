1dfd40574e16cfc1f91132881e36d130a116b2d1
Thomas Hopf <thomas.hopf@gmail.com>
2016-12-08 16:10:37 -0500

    Fixed: disabled import of broken file

--- evcouplings/utils/__init__.py
+++ evcouplings/utils/__init__.py
@@ -2,6 +2,6 @@ from evcouplings.utils.batch import *
 from evcouplings.utils.calculations import *
 from evcouplings.utils.config import *
 from evcouplings.utils.helpers import *
-from evcouplings.utils.parallelize import *
+# from evcouplings.utils.parallelize import *
 from evcouplings.utils.plots import *
 from evcouplings.utils.system import *
