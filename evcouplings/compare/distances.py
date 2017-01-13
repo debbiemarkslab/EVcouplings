a0b05cd4015e96e6e34e1e46776b4a40ab364aaa
Thomas Hopf <thomas.hopf@gmail.com>
2017-01-13 12:27:27 -0500

    Removed old code, added distance calculation function

--- evcouplings/compare/distances.py
+++ evcouplings/compare/distances.py
@@ -1,37 +1,76 @@
 """
-Distance calculations in PDB 3D structures
+Distance calculations on PDB 3D coordinates
 
 Authors:
   Thomas A. Hopf
 """
 
 import numpy as np
-import pandas as pd
 from numba import jit
 
 
-def res_name_to_tuple(name):
+@jit(nopython=True)
+def _distances(residues_i, coords_i, residues_j, coords_j, symmetric):
     """
-    Transform PDB residue index into tuple of
-    position and insertion code
+    Compute minimum atom distances between residues. If used on
+    a single atom per residue, this function can e.g. also compute
+    C_alpha distances.
 
     Parameters
     ----------
-    name : str
-        Residue name (e.g. "188" or "189A")
+    residues_i : np.array
+        Matrix of size N_i x 2, where N_i = number of residues
+        in PDB chain used for first axis. Each row of this
+        matrix contains the first and last (inclusive) index
+        of the atoms comprising this residue in the coords_i
+        matrix
+    coords_i : np.array
+        N_a x 3 matrix containing 3D coordinates of all atoms
+        (where N_a is total number of atoms in chain)
+    residues_j : np.array
+        Like residues_i, but for chain used on second axis
+    coords_j : np.array
+        Like coords_j, but for chain used on second axis
+
     Returns
     -------
-    tuple(int, str)
-        Residue name split into position index
-        and insertion code (empty string if residue
-        has no insertion code)
+    dists : np.array
+        Matrix of size N_i x N_j containing minimum atom
+        distance between residue i and j in dists[i, j]
     """
-    if len(name) < 1:
-        raise ValueError(
-            "Residue name must at least be one character long."
-        )
+    LARGE_DIST = 1000000
 
-    if name[-1].isdigit():
-        return (int(name), "")
-    else:
-        return (int(name[:-1]), name[-1])
+    N_i, _ = residues_i.shape
+    N_j, _ = residues_j.shape
+
+    # matrix to hold final distances
+    dists = np.zeros((N_i, N_j))
+
+    # iterate all pairs of residues
+    for i in range(N_i):
+        for j in range(N_j):
+            # limit computation in symmetric case and
+            # use previously calculated distance
+            if symmetric and i >= j:
+                dists[i, j] = dists[j, i]
+            else:
+                range_i = residues_i[i]
+                range_j = residues_j[j]
+                min_dist = LARGE_DIST
+
+                # iterate all pairs of atoms for residue pair;
+                # end of coord range is inclusive, so have to add 1
+                for a_i in range(range_i[0], range_i[1] + 1):
+                    for a_j in range(range_j[0], range_j[1] + 1):
+                        # compute Euclidean distance between atom pair
+                        cur_dist = np.sqrt(
+                            np.sum(
+                                (coords_i[a_i] - coords_j[a_j]) ** 2
+                            )
+                        )
+                        # store if this is a smaller distance
+                        min_dist = min(min_dist, cur_dist)
+
+                dists[i, j] = min_dist
+
+    return dists
