93e129d9a982e2183ef85921b70026a96bcd7c5a
Thomas Hopf <thomas.hopf@gmail.com>
2017-01-18 16:37:15 -0500

    Added: module import

---
+++ evcouplings/compare/__init__.py
@@ -0,0 +1,5 @@
+from evcouplings.compare.ecs import *
+from evcouplings.compare.distances import *
+from evcouplings.compare.mapping import *
+from evcouplings.compare.pdb import *
+from evcouplings.compare.sifts import *
