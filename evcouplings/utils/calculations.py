7a0c2cc9db4bad6f0683ae4422e941cba2ad67c7
Thomas Hopf <thomas.hopf@gmail.com>
2017-03-17 16:45:08 -0400

    Improved: restructured entropy calculation functions

--- evcouplings/utils/calculations.py
+++ evcouplings/utils/calculations.py
@@ -34,8 +34,40 @@ def entropy(X, normalize=False):
         return H
 
 
+def entropy_vector(model, normalize=True):
+    """
+    Compute vector of positional entropies for
+    single-site frequencies in a CouplingsModel
+
+    Parameters
+    ----------
+    model : CouplingsModel
+        Model for which entropy of sequence
+        alignment will be computed (based on
+        single-site frequencies f_i(A_i)
+        contained in model)
+    normalize : bool, default: True
+        Normalize entropy to range 0 (variable)
+        to 1 (conserved) instead of raw values
+
+    Returns
+    -------
+    np.array
+        Vector of length model.L containing
+        entropy for each position
+    """
+    cons = np.apply_along_axis(
+        lambda x: entropy(x, normalize=normalize),
+        axis=1, arr=model.fi()
+    )
+
+    return cons
+
 def entropy_map(model, normalize=True):
     """
+    Compute dictionary of positional entropies for
+    single-site frequencies in a CouplingsModel
+
     Parameters
     ----------
     model : CouplingsModel
@@ -53,10 +85,7 @@ def entropy_map(model, normalize=True):
         Map from positions in sequence (int) to
         entropy of column (float) in alignment
     """
-    cons = np.apply_along_axis(
-        lambda x: entropy(x, normalize=normalize),
-        axis=1, arr=model.fi()
-    )
+    cons = entropy_vector(model, normalize)
 
     return dict(
         zip(model.index_list, cons)
