3391d5d9ab3b43b4b15d1bf10317148d362bcfc3
Thomas Hopf <thomas.hopf@gmail.com>
2017-03-29 12:11:29 -0400

    Improved: run_cns handles command strings and files

--- evcouplings/fold/tools.py
+++ evcouplings/fold/tools.py
@@ -6,13 +6,15 @@ Authors:
   Thomas A. Hopf
 """
 
+from evcouplings.utils.config import InvalidParameterError
 from evcouplings.utils.system import (
     run, valid_file, create_prefix_folders,
     verify_resources, ResourceError
 )
 
 
-def run_cns(input_script, log_file=None, source_script=None, binary="cns"):
+def run_cns(inp_script=None, inp_file=None, log_file=None,
+            source_script=None, binary="cns"):
     """
     Run CNSsolve 1.3
 
@@ -20,10 +22,15 @@ def run_cns(input_script, log_file=None, source_script=None, binary="cns"):
     of CNS, since their paths are determined by .inp scripts and
     hard to check automatically and in a general way.
 
+    Either input_script or input_file has to be specified.
+
     Parameters
     ----------
-    input_script : str
-        Path to .inp input script
+    inp_script : str, optional (default: None)
+        CNS ".inp" input script (actual commands, not file)
+    inp_file : str, optional (default: None)
+        Path to .inp input script file. Will override
+        inp_script if also specified.
     log_file : str, optional (default: None)
         Save CNS stdout output to this file
     source_script : str, optional (default: None)
@@ -48,12 +55,20 @@ def run_cns(input_script, log_file=None, source_script=None, binary="cns"):
 
     cmd += binary
 
+    if inp_script is None and inp_file is None:
+        raise InvalidParameterError(
+            "Must specify either input_script or input_file"
+        )
+
     # read input script, this is fed into CNS using stdin
-    with open(input_script) as f:
-        inp = "".join(f.readlines())
+    if inp_file is not None:
+        with open(inp_file) as f:
+            inp_script = "".join(f.readlines())
 
     # run and store output
-    return_code, stdout, stderr = run(cmd, stdin=inp, shell=True)
+    return_code, stdout, stderr = run(
+        cmd, stdin=inp_script, shell=True
+    )
 
     # write stdout output to log file
     if log_file is not None:
