0a8081e50ed79462f514654c870f39ee76ae7208
Thomas Hopf <thomas.hopf@gmail.com>
2017-04-03 14:25:15 -0400

    Added: folding protocol skeleton

--- evcouplings/fold/__init__.py
+++ evcouplings/fold/__init__.py
@@ -2,3 +2,4 @@ from evcouplings.fold.tools import *
 from evcouplings.fold.cns import *
 from evcouplings.fold.filter import *
 from evcouplings.fold.restraints import *
+from evcouplings.fold.protocol import *
---
+++ evcouplings/fold/protocol.py
@@ -0,0 +1,99 @@
+"""
+Protocols for predicting protein 3D structure from ECs
+
+Authors:
+  Thomas A. Hopf
+"""
+
+from evcouplings.utils.config import (
+    check_required, InvalidParameterError
+)
+from evcouplings.utils.system import (
+    create_prefix_folders, verify_resources
+)
+
+
+def cns_dgsa(**kwargs):
+    """
+    Protocol:
+    Compare ECs for single proteins (or domains)
+    to 3D structure information
+
+    Parameters
+    ----------
+    Mandatory kwargs arguments:
+        See list below in code where calling check_required
+
+    Returns
+    -------
+    outcfg : dict
+        Output configuration of the pipeline, including
+        the following fields:
+
+        mutation_matrix_file
+        [mutation_dataset_predicted_file]
+    """
+    check_required(
+        kwargs,
+        [
+            "prefix", "ec_file", "target_sequence_file",
+            "segments", "config_file",
+        ]
+    )
+
+    # make sure model file exists
+    verify_resources(
+        "Model parameter file does not exist",
+        kwargs["model_file"]
+    )
+
+    prefix = kwargs["prefix"]
+
+    # make sure output directory exists
+    create_prefix_folders(prefix)
+
+    outcfg = {}
+
+    # TODO: allow to reuse secondary structure,
+    # TODO: or have external file;
+    # TODO: create indextable-like file here
+    # TODO: implement folding protocol here
+    # TODO: cut to different parts of query sequence
+
+    return outcfg
+
+
+# list of available folding protocols
+PROTOCOLS = {
+    # standard EVfold protocol
+    "cns_dgsa": cns_dgsa,
+}
+
+
+def run(**kwargs):
+    """
+    Run mutation protocol
+
+    Parameters
+    ----------
+    Mandatory kwargs arguments:
+        protocol: Folding protocol to run
+        prefix: Output prefix for all generated files
+
+    Returns
+    -------
+    outcfg : dict
+        Output configuration of stage
+        (see individual protocol for fields)
+    """
+    check_required(kwargs, ["protocol"])
+
+    if kwargs["protocol"] not in PROTOCOLS:
+        raise InvalidParameterError(
+            "Invalid protocol selection: " +
+            "{}. Valid protocols are: {}".format(
+                kwargs["protocol"], ", ".join(PROTOCOLS.keys())
+            )
+        )
+
+    return PROTOCOLS[kwargs["protocol"]](**kwargs)
