2399cfee75c6da1f6b70d67c54b467d7a53498c1
Thomas Hopf <thomas.hopf@gmail.com>
2017-04-17 19:49:12 -0400

    Fixed: missing import in fold init

--- evcouplings/fold/__init__.py
+++ evcouplings/fold/__init__.py
@@ -3,3 +3,4 @@ from evcouplings.fold.cns import *
 from evcouplings.fold.filter import *
 from evcouplings.fold.restraints import *
 from evcouplings.fold.protocol import *
+from evcouplings.fold.ranking import *
