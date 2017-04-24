4df3888d15b55d008a36d7aee34f7a398816c951
Thomas Hopf <thomas.hopf@gmail.com>
2017-04-24 15:53:37 -0400

    Added: import for pymol scripts

--- evcouplings/visualize/__init__.py
+++ evcouplings/visualize/__init__.py
@@ -2,3 +2,5 @@ from evcouplings.visualize.couplings import *
 from evcouplings.visualize.pairs import *
 from evcouplings.visualize.mutations import *
 from evcouplings.visualize.misc import *
+from evcouplings.visualize.pymol import *
+
