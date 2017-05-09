11d29883336f8f86059d5c4f329197d3f2d7d0fc
Thomas Hopf <thomas.hopf@gmail.com>
2017-05-09 14:44:04 -0400

    Finished basic pymol visualization functions

--- evcouplings/visualize/pymol.py
+++ evcouplings/visualize/pymol.py
@@ -6,28 +6,58 @@ Authors:
   Thomas A. Hopf
 """
 
+import pandas as pd
+
+
+def _write_pymol_commands(commands, output_file):
+    """
+    Helper function to write a set of lines to a file
+
+    Parameters
+    ----------
+    commands : list(str)
+        List of commands, each will be written to a new
+        line.
+    output_file : str or file-like:
+        Path to output file (str), or writeable
+        file handle, in which commands will be stored
+    """
+    cmd_str = "\n".join(commands)
+
+    try:
+        output_file.write(cmd_str)
+    except AttributeError:
+        with open(output_file, "w") as f:
+            f.write(cmd_str)
+
 
 def pymol_secondary_structure(residues, output_file, chain=None,
                               sec_struct_column="sec_struct_3state"):
     """
     Assign predicted secondary structure to 3D structure in Pymol
-    
+
     Parameters
     ----------
     residues : pandas.DataFrame
         Table with secondary structure prediction/assignment
         (position in column i)
-    output_file : str
-        Write Pymol script to this file
+    output_file : str or file-like
+        Write Pymol script to this file (filename as string,
+        or writeable file handle)
     chain : str, optional (default: None)
         PDB chain that should be targeted by secondary
         structure assignment. If None, residues will be selected
         py position alone, which may cause wrong assignments
         if multiple chains are present in the structure.
     sec_struct_column : str, optional (default: sec_struct_3state)
-        Column in residues table that contains the secondary 
+        Column in residues table that contains the secondary
         structure symbols for each position (convention: helix: H,
         strand: E, other: C)
+
+    Returns
+    -------
+    cmds : list
+        List of generated pymol commands (written to output_file)
     """
     cmds = []
 
@@ -60,14 +90,14 @@ def pymol_secondary_structure(residues, output_file, chain=None,
     # the changes are displayed
     cmds.append("rebuild")
 
-    with open(output_file, "w") as f:
-        f.write("\n".join(cmds))
+    _write_pymol_commands(cmds, output_file)
+    return cmds
 
 
 def pymol_pair_lines(pairs, output_file, chain=None, atom="CA", pair_prefix="ec"):
     """
     Draw lines between residue pairs on a structure using Pymol
-    
+
     Parameters
     ----------
     pairs : pandas.DataFrame
@@ -86,12 +116,17 @@ def pymol_pair_lines(pairs, output_file, chain=None, atom="CA", pair_prefix="ec"
         - If None, residues will be selected
           py position alone, which may cause wrong assignments
           if multiple chains are present in the structure.
-        - Different chains can be assigned for each i and j, 
+        - Different chains can be assigned for each i and j,
           if a dictionary that maps from segment (str) to PDB chain (str)
           is given. In this case, columns "segment_i" and "segment_j"
           must be present in the pairs dataframe.
     atom : str, optional (default: "CA")
         Put end of line on this atom in each residue
+
+    Returns
+    -------
+    cmds : list
+        List of generated pymol commands (written to output_file)
     """
     cmds = []
 
@@ -138,12 +173,78 @@ def pymol_pair_lines(pairs, output_file, chain=None, atom="CA", pair_prefix="ec"
                     "set {}, {}, {}".format(param, r[param], id_)
                 )
 
-    cmd_str = "\n".join(cmds)
+    _write_pymol_commands(cmds, output_file)
+    return cmds
 
-    try:
-        output_file.write(cmd_str)
-    except AttributeError:
-        with open(output_file, "w") as f:
-            f.write(cmd_str)
 
+def pymol_mapping(mapping, output_file, chain=None, atom=None):
+    """
+    Map properties onto individual residues of
+    structure using Pymol
+
+    Parameters
+    ----------
+    mapping : pandas.DataFrame
+        Table of properties to map on residues.
+        * positions in column "i"
+        * color in column "color" (string with
+        Pymol color name or hexadecimal code prefixed by 0x
+        instead of #)
+        * display type in column "show"
+        * b factor in column "b_factor"
+    output_file : str or file-like
+        Write Pymol script to this file (filename as string,
+        or writeable file handle)
+    chain : str, optional (default: None)
+        PDB chain that should be targeted by secondary
+        structure assignment. If None, residues will be selected
+        py position alone, which may cause wrong assignments
+        if multiple chains are present in the structure.
+    atom : str, optional (default: None)
+        Only apply mapping to this atom in each residue
+
+    Returns
+    -------
+    cmds : list
+        List of generated pymol commands (written to output_file)
+    """
+    cmds = []
+
+    # define chain selector if chain is given
+    if chain is not None:
+        chain_sel = " and chain '{}'".format(chain)
+    else:
+        chain_sel = ""
+
+    # define atom selector, if given
+    if atom is not None:
+        atom_sel = " and name {}".format(atom)
+    else:
+        atom_sel = ""
+
+    # create Pymol commands for each row in mapping table
+    for i, (idx, r) in enumerate(mapping.iterrows(), start=1):
+        sel = "resid {}{}{}".format(
+            r["i"], chain_sel, atom_sel
+        )
+
+        # adjust color, display type and b_factor if
+        # the respective value is given (i.e., not nan)
+        if "color" in r and pd.notnull(r["color"]):
+            cmds.append(
+                "color {}, {}".format(r["color"], sel)
+            )
+
+        if "show" in r and pd.notnull(r["show"]):
+            cmds.append(
+                "show {}, {}".format(r["show"], sel)
+            )
+
+        if "b_factor" in r and pd.notnull(r["b_factor"]):
+            cmds.append(
+                "alter {}, b={}".format(sel, r["b_factor"])
+            )
+            pass
+
+    _write_pymol_commands(cmds, output_file)
     return cmds
