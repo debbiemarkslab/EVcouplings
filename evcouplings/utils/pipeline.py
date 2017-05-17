16d7f23a5cd4ce003b10e1fbb3dfa06c864557c4
Thomas Hopf <thomas.hopf@gmail.com>
2017-05-17 15:51:45 -0400

    Added: pipeline can delete output files at the end. Closes #27

--- evcouplings/utils/pipeline.py
+++ evcouplings/utils/pipeline.py
@@ -217,6 +217,9 @@ def execute(**config):
     create_archive(config, global_state, archive_file)
     global_state["archive_file"] = archive_file
 
+    # delete selected output files if requested
+    global_state = delete_outputs(config, global_state)
+
     # write final global state of pipeline
     write_config_file(
         prefix + FINAL_CONFIG_SUFFIX, global_state
@@ -239,7 +242,7 @@ def create_archive(config, outcfg, output_file):
         config["management"]["archive"] (list of key 
         used to index outcfg) to determine
         which files should be added to archive
-    outcfg : dict-llike
+    outcfg : dict-like
         Output configuration of job
     output_file : str
         Store archive file to this path
@@ -268,6 +271,61 @@ def create_archive(config, outcfg, output_file):
             else:
                 if valid_file(outcfg[k]):
                     tar.add(outcfg[k])
+
+
+def delete_outputs(config, outcfg):
+    """
+    Remove pipeline outputs to save memory
+    after running the job
+
+    Parameters
+    ----------
+    config : dict-like
+        Input configuration of job. Uses 
+        config["management"]["delete"] (list of key 
+        used to index outcfg) to determine
+        which files should be added to archive
+    outcfg : dict-like
+        Output configuration of job
+    
+    Returns
+    -------
+    outcfg_cleaned : dict-like
+        Output configuration with selected
+        output keys removed.
+    """
+    # determine keys (corresponding to files) in
+    # outcfg that should be stored
+    outkeys = config.get("management").get("delete")
+
+    # if no output keys are requested, nothing to do
+    if outkeys is None or len(outkeys) == 0:
+        return outcfg
+
+    # go through all flagged files and delete if existing
+    for k in outkeys:
+        # skip missing keys or ones not defined
+        if k not in outcfg or k is None:
+            continue
+
+        # delete list of files
+        if k.endswith("files"):
+            for f in outcfg[k]:
+                try:
+                    os.remove(f)
+                except OSError:
+                    pass
+            del outcfg[k]
+
+        # delete individual file
+        else:
+            try:
+                os.remove(outcfg[k])
+                del outcfg[k]
+            except OSError:
+                pass
+
+    return outcfg
 
 
 def verify_prefix(verify_subdir=True, **config):
