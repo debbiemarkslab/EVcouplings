4185fb0cfc24b8ebb0a8396fe583a938f84cdb2f
Benjamin Schubert <benni.schubert@gmail.com>
2017-08-28 10:16:29 -0400

    [INTERNAL] renaming constant to all upper-case and all variables to all lower-case.

--- evcouplings/utils/update_database.py
+++ evcouplings/utils/update_database.py
@@ -16,19 +16,19 @@ import click
 from evcouplings.compare import SIFTS
 from evcouplings.utils import Progressbar
 
-Uniprot_url = "ftp.uniprot.org"
-Uniprot_cwd = "/pub/databases/uniprot/current_release/knowledgebase/complete/"
-Uniprot_file = "uniprot_{type}.fasta.gz"
+UNIPROT_URL = "ftp.uniprot.org"
+UNIPROT_CWD = "/pub/databases/uniprot/current_release/knowledgebase/complete/"
+UNIPROT_FILE = "uniprot_{type}.fasta.gz"
 
-DB_url = "ftp.uniprot.org"
-DB_cwd = "/pub/databases/uniprot/uniref/{type}/"
-DB_file = "{type}.fasta.gz"
+DB_URL = "ftp.uniprot.org"
+DB_CWD = "/pub/databases/uniprot/uniref/{type}/"
+DB_FILE = "{type}.fasta.gz"
 
-DB_suffix = "{type}_{year}_{month}.fasta"
-DB_current = "{type}_current.fasta"
+DB_SUFFIX = "{type}_{year}_{month}.fasta"
+DB_CURRENT = "{type}_current.fasta"
 
-SIFTS_suffix = "pdb_chain_uniprot_plus_{year}_{month}_{day}.{extension}"
-SIFTS_current = "pdb_chain_uniprot_plus_current.{extension}"
+SIFTS_SUFFIX = "pdb_chain_uniprot_plus_{year}_{month}_{day}.{extension}"
+SIFTS_CURRENT = "pdb_chain_uniprot_plus_current.{extension}"
 
 
 def symlink_force(target, link_name):
@@ -112,8 +112,8 @@ def run(**kwargs):
         print("Updating SIFTS")
 
     SIFTS_dir = kwargs.get("sifts", os.path.realpath(__file__))
-    sifts = os.path.join(SIFTS_dir, SIFTS_suffix)
-    sifts_curr = os.path.join(SIFTS_dir, SIFTS_current)
+    sifts = os.path.join(SIFTS_dir, SIFTS_SUFFIX)
+    sifts_curr = os.path.join(SIFTS_dir, SIFTS_CURRENT)
     sifts_table = sifts.format(year=year, month=month, day=day, extension="csv")
     sifts_fasta = sifts.format(year=year, month=month, day=day, extension="fasta")
     s_new = SIFTS(sifts.format(year=year, month=month, day=day, extension="csv"))
@@ -125,7 +125,7 @@ def run(**kwargs):
         symlink_force(sifts_fasta, sifts_curr.format(extension="fasta"))
 
     # update uniref
-    DB_path = kwargs.get("db", os.path.realpath(__file__))
+    db_path = kwargs.get("db", os.path.realpath(__file__))
     for db_type in ["uniref100", "uniref90", "uniprot"]:
 
         if verbose:
@@ -133,34 +133,34 @@ def run(**kwargs):
 
         if db_type == "uniprot":
             # download Swiss and TrEMBL and concatinate both
-            out_path = os.path.join(DB_path, DB_suffix.format(type=db_type, year=year, month=month))
-            db_curr = os.path.join(DB_path, DB_current.format(type=db_type))
+            out_path = os.path.join(db_path, DB_SUFFIX.format(type=db_type, year=year, month=month))
+            db_curr = os.path.join(db_path, DB_CURRENT.format(type=db_type))
             for i, type_d in enumerate(["sprot", "trembl"]):
                 if i:
-                    file_url = Uniprot_file.format(type=type_d)
-                    download_ftp_file(Uniprot_url, Uniprot_cwd, file_url, out_path, file_handling="ab", verbose=verbose)
+                    file_url = UNIPROT_FILE.format(type=type_d)
+                    download_ftp_file(UNIPROT_URL, UNIPROT_CWD, file_url, out_path, file_handling="ab", verbose=verbose)
                 else:
-                    file_url = Uniprot_file.format(type=type_d)
-                    download_ftp_file(Uniprot_url, Uniprot_cwd, file_url, out_path, verbose=verbose)
+                    file_url = UNIPROT_FILE.format(type=type_d)
+                    download_ftp_file(UNIPROT_URL, UNIPROT_CWD, file_url, out_path, verbose=verbose)
         else:
             # download uniref db
-            db_file = DB_file.format(type=db_type)
-            db_cwd = DB_cwd.format(type=db_type)
-            out_path = os.path.join(DB_path, DB_suffix.format(type=db_type, year=year, month=month))
-            db_curr = os.path.join(DB_path, DB_current.format(type=db_type))
-            download_ftp_file(DB_url, db_cwd, db_file, out_path, verbose=verbose)
+            db_file = DB_FILE.format(type=db_type)
+            db_cwd = DB_CWD.format(type=db_type)
+            out_path = os.path.join(db_path, DB_SUFFIX.format(type=db_type, year=year, month=month))
+            db_curr = os.path.join(db_path, DB_CURRENT.format(type=db_type))
+            download_ftp_file(DB_URL, db_cwd, db_file, out_path, verbose=verbose)
 
         if symlink:
             symlink_force(out_path, db_curr)
 
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 # run settings
 @click.option("-s", "--sifts", default="/groups/marks/databases/SIFTS/", help="SIFTS output directory")
-@click.option("-d", "--db", default="/groups/marks/databases/jackhmmer/", help="SIFTS output directory")
+@click.option("-d", "--db", default="/groups/marks/databases/jackhmmer/", help="Uniprot output directory")
 @click.option("-l", "--symlink", default=False, is_flag=True,
               help="Creates symlink with ending '_current.' pointing to the newly created db files")
 @click.option("-v", "--verbose", default=False, is_flag=True, help="Enables verbose output")
