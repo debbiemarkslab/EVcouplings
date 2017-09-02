0973395b57ac52c0bc095dd1beb8b28548ce1ece
Benjamin Schubert <benni.schubert@gmail.com>
2017-09-02 15:53:22 -0400

    [INTERNAL] added multiple other modules to mock

--- docs/conf.py
+++ docs/conf.py
@@ -23,14 +23,14 @@ sys.path.insert(0, os.path.abspath('..'))
 
 
 # C dependent libraries have to be mocked. Otherwise RTD does not work
-from unittest.mock import MagicMock
+from unittest.mock import Mock
 
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-            return MagicMock()
+# class Mock(MagicMock):
+#     @classmethod
+#     def __getattr__(cls, name):
+#             return MagicMock()
 
-MOCK_MODULES = ['biopython','ruamel', 'ruamel.yaml', 'ruamel', 'mmtf-python','llvmlite','pyyaml',
+MOCK_MODULES = ['biopython', 'ruamel', 'ruamel.yaml', 'mmtf-python', 'pyyaml','sqlalchemy',
                 'mmtf','matplotlib', 'numba', 'scipy', 'click', 'argparse', 'numpy', 'pandas']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
