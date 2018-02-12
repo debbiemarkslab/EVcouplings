d062e14227821acc330b6473818ae835d5e001fc
Benjamin Schubert <benni.schubert@gmail.com>
2018-02-12 11:04:13 -0500

    [TEST] App unittests

---
+++ test/TestUtilsApp.py
@@ -0,0 +1,7 @@
+import os
+import unittest
+from unittest import TestCase
+from evcouplings.utils import SubmitterFactory, Command
+from evcouplings.utils.app import app
+
+class TestUtilsAppp(TestCase):
