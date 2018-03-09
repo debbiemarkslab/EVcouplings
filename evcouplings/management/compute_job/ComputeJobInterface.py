5510fe6733f86d7d5cd749ffd2155c6809109e9b
Christian Dallago <code@dallago.us>
2018-03-09 15:08:13 -0500

    add abstract methods

--- evcouplings/management/compute_job/ComputeJobInterface.py
+++ evcouplings/management/compute_job/ComputeJobInterface.py
@@ -6,11 +6,14 @@ class ComputeJobInterface(object, metaclass=abc.ABCMeta):
     def __init__(self, config):
         self.config = config
 
+    @abc.abstractmethod
     def update_job_status(self, status=None, stage=None):
         raise NotImplementedError
 
+    @abc.abstractmethod
     def get_jobs_from_group(self):
         raise NotImplementedError
 
+    @abc.abstractmethod
     def get_job(self):
         raise NotImplementedError
--- evcouplings/management/dumper/ResultsDumperInterface.py
+++ evcouplings/management/dumper/ResultsDumperInterface.py
@@ -6,20 +6,26 @@ class ResultsDumperInterface(object, metaclass=abc.ABCMeta):
     def __init__(self, config):
         self.config = config
 
+    @abc.abstractmethod
     def write_tar(self):
         raise NotImplementedError
 
+    @abc.abstractmethod
     def tar_path(self):
         raise NotImplementedError
 
+    @abc.abstractmethod
     def download_tar(self):
         raise NotImplementedError
 
+    @abc.abstractmethod
     def write_files(self):
         raise NotImplementedError
 
+    @abc.abstractmethod
     def write_file(self, file_path):
         raise NotImplementedError
 
+    @abc.abstractmethod
     def clear(self):
         raise NotImplementedError
