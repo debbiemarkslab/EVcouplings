49748ad4ac28d5c0a152cb65cdc3f90abfb74569
thomashopf <thomashopf@users.noreply.github.com>
2020-09-27 17:24:45 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     name='evcouplings',
 
     # Version:
-    version='0.0.5',
+    version='0.1.0',
 
     description='A Framework for evolutionary couplings analysis',
     long_description=readme,
