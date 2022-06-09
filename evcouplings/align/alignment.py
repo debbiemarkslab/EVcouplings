4f580c1ba980eb61f16a6aefebc0f9d34d8b3a8e
Lood <loodvn@gmail.com>
2022-06-09 17:47:52 -0400

    added parallel numba option, that ignores gaps and doesn't treat them as equal in sequence similarity

--- evcouplings/align/alignment.py
+++ evcouplings/align/alignment.py
@@ -11,7 +11,7 @@ from collections import namedtuple, OrderedDict, defaultdict
 from copy import deepcopy
 
 import numpy as np
-from numba import jit
+from numba import jit, prange
 
 from evcouplings.utils.calculations import entropy
 from evcouplings.utils.helpers import DefaultOrderedDict, wrap
@@ -1208,5 +1208,54 @@ def num_cluster_members(matrix, identity_threshold):
             if pair_id / L >= identity_threshold:
                 num_neighbors[i] += 1
                 num_neighbors[j] += 1
+
+    return num_neighbors
+
+@jit(nopython=True)
+def num_cluster_members_parallel(matrix, identity_threshold, invalid_value):
+    """
+    Calculate number of sequences in alignment
+    within given identity_threshold of each other
+
+    Parameters
+    ----------
+    matrix : np.array
+        N x L matrix containing N sequences of length L.
+        Matrix must be mapped to range(0, num_symbols) using
+        map_matrix function
+    identity_threshold : float
+        Sequences with at least this pairwise identity will be
+        grouped in the same cluster.
+
+    Returns
+    -------
+    np.array
+        Vector of length N containing number of cluster
+        members for each sequence (inverse of sequence
+        weight)
+    """
+    N, L = matrix.shape
+    L = 1.0 * L
+
+    # minimal cluster size is 1 (self)
+    num_neighbors = np.ones((N))
+    L_non_gaps = L - np.sum(matrix == invalid_value, axis=1)  # Edit: From EVE, use the non-gapped length
+
+    # compare all pairs of sequences
+    # Edit: Rewrote loop without any dependencies between inner and outer loops, so that it can be parallelized
+    for i in prange(N):
+        num_neighbors_i = 1  # num_neighbors_i = 0  # TODO why did I make this 0 again?
+        for j in range(N):
+            if i == j:
+                continue
+            pair_matches = 0
+            for k in range(L):  # This should hopefully be vectorised by numba
+                if matrix[i, k] == matrix[j, k] and matrix[i, k] != invalid_value:  # Edit(Lood): Don't count gaps as matches
+                    pair_matches += 1
+            # Edit(Lood): Calculate identity as fraction of non-gapped positions (so this similarity is asymmetric)
+            if pair_matches / L_non_gaps[i] >= identity_threshold:
+                num_neighbors_i += 1
+
+        num_neighbors[i] = num_neighbors_i
 
     return num_neighbors
---
+++ test/TestAlignment.py
@@ -0,0 +1,82 @@
+import unittest
+from unittest import TestCase
+
+import numpy as np
+
+from evcouplings.align import num_cluster_members, num_cluster_members_parallel
+
+
+class TestWeights(TestCase):
+
+    def test_num_cluster_members_basic(self):
+        # Basic test
+        invalid_value = -1  # Usually the matrix has to be between (0, num_symbols) but here we can do what we want
+
+        # Symmetric and all equal
+        matrix = np.array(
+            [[0, 1, 2, -1],
+             [0, 1, 2, -1],
+             [0, 1, 2, -1]]
+        )
+        expected = np.array([3, 3, 3])
+        # num_neighbours = num_cluster_members(matrix, identity_threshold=0.8)  #, invalid_value=invalid_value
+        num_neighbours = num_cluster_members_parallel(matrix, identity_threshold=0.8, invalid_value=invalid_value)
+        self.assertTrue(np.array_equal(num_neighbours, expected), "Expected: {}\nGot: {}".format(expected, num_neighbours))
+
+        # Just below 0.5 sequence similarity threshold
+        # Gaps not counted, so 1/3 similarity < 0.5
+        matrix = np.array(
+            [[0, 1, 2, -1],
+             [0, 0, 0, -1]]
+        )
+        expected = np.array([1, 1])
+        num_neighbours = num_cluster_members_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
+        self.assertTrue(np.array_equal(num_neighbours, expected),
+                        "Expected: {}\nGot: {}".format(expected, num_neighbours))
+
+        # Mostly gaps, so sequences are just [0] and [1]
+        matrix = np.array(
+            [[0, -1, -1, -1, -1],
+             [1, -1, -1, -1, -1]]
+        )
+        expected = np.array([1, 1])
+        num_neighbours = num_cluster_members_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
+        self.assertTrue(np.array_equal(num_neighbours, expected),
+                        "Expected: {}\nGot: {}".format(expected, num_neighbours))
+
+        # Not counting its own gaps makes the threshold asymmetric
+        # Sequence A: only [2,3] our of 5 overlap, so 0.4 < 0.5, so sequence on its own
+        # Sequence B: Only [2,3] out of 3 non-gap overlap, so 0.667 > 0.5 and seq A is a neighbour of seq B
+        matrix = np.array(
+            [[0, 1, 2, 3, 0, 0],
+             [-1, -1, 2, 3, 4, -1]]
+        )
+        expected = np.array([1, 2])
+        num_neighbours = num_cluster_members_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
+        self.assertTrue(np.array_equal(num_neighbours, expected),
+                        "Expected: {}\nGot: {}".format(expected, num_neighbours))
+
+    def test_num_cluster_fragment(self):
+        invalid_value = -1  # Usually the matrix has to be between (0, num_symbols) but here we can do what we want
+        # Problem: N sequences appear in cluster together (so num_cluster_members is N) but a fragment appears as its own cluster
+
+        matrix = np.array(
+            [[0, 1, 2, 3, 4, 5, 6],
+             [0, 1, 2, 3, 4, 5, 7],
+             [0, 1, 1, 3, 4, 5, 6],
+             [0, 1, 2, -1, -1, -1, -1],  # Fragment
+             ]
+        )
+        # Note: The fragment isn't a neighbour of the full sequences, but the full sequences are neighbours of the fragment
+        expected = np.array([3, 3, 3, 4])
+        num_neighbours = num_cluster_members_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
+        self.assertTrue(np.array_equal(num_neighbours, expected),
+                        "Expected: {}\nGot: {}".format(expected, num_neighbours))
+
+    def test_num_cluster_full(self):
+        """TODO Test on a full MSA, running alignment etc, just to verify"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
