6efc8cf08d17106b4adf23db8fe6258d373c91e7
Lood <loodvn@gmail.com>
2022-06-13 17:42:29 -0400

    changed sequence_weight_strategy name to "uniform" using AA letters instead of numbers for tests

--- evcouplings/align/alignment.py
+++ evcouplings/align/alignment.py
@@ -881,7 +881,7 @@ class Alignment:
         clustering all sequences with sequence identity
         greater or equal to the given threshold.
 
-        Can select either (nogaps (default), legacy or identity (no weights calculated)).
+        Can select either (nogaps (default), legacy or uniform (no weights calculated)).
 
         .. note::
 
@@ -902,17 +902,17 @@ class Alignment:
         self.__ensure_mapped_matrix()
 
         if weights_calc_method == "nogaps":
-            self.num_cluster_members = num_cluster_members_parallel(
+            self.num_cluster_members = num_cluster_members_nogaps_parallel(
                 self.matrix_mapped, identity_threshold
             )
         elif weights_calc_method == "legacy":
             self.num_cluster_members = num_cluster_members_legacy(
                 self.matrix_mapped, identity_threshold
             )
-        elif weights_calc_method == "identity":
+        elif weights_calc_method == "uniform":
             self.num_cluster_members = np.ones(self.N)
         else:
-            raise ValueError("Invalid weights_calc_method: {}. Must be one of {}".format(weights_calc_method, ["nogaps", "legacy", "identity"]))
+            raise ValueError("Invalid weights_calc_method: {}. Must be one of {}".format(weights_calc_method, ["nogaps", "legacy", "uniform"]))
 
         self.weights = 1.0 / self.num_cluster_members
 
@@ -1225,8 +1225,8 @@ def num_cluster_members_legacy(matrix, identity_threshold):
     return num_neighbors
 
 
-@jit(nopython=True)
-def num_cluster_members_parallel(matrix, identity_threshold, invalid_value):
+@jit(nopython=True, parallel=True)
+def num_cluster_members_nogaps_parallel(matrix, identity_threshold, invalid_value):
     """
     Calculate number of sequences in alignment
     within given identity_threshold of each other
@@ -1240,7 +1240,8 @@ def num_cluster_members_parallel(matrix, identity_threshold, invalid_value):
     identity_threshold : float
         Sequences with at least this pairwise identity will be
         grouped in the same cluster.
-
+    invalid_value : int
+        Value in matrix that is considered invalid, e.g. gap or lowercase character.
     Returns
     -------
     np.array
@@ -1258,7 +1259,58 @@ def num_cluster_members_parallel(matrix, identity_threshold, invalid_value):
     # compare all pairs of sequences
     # Edit: Rewrote loop without any dependencies between inner and outer loops, so that it can be parallelized
     for i in prange(N):
-        num_neighbors_i = 1  # num_neighbors_i = 0  # TODO why did I make this 0 again?
+        num_neighbors_i = 1  # num_neighbors_i = 0  # TODO why did I make this 0 again? Probably because I thought I'd have to count i == j
+        for j in range(N):
+            if i == j:
+                continue
+            pair_matches = 0
+            for k in range(L):  # This should hopefully be vectorised by numba
+                if matrix[i, k] == matrix[j, k] and matrix[i, k] != invalid_value:  # Edit(Lood): Don't count gaps as matches
+                    pair_matches += 1
+            # Edit(Lood): Calculate identity as fraction of non-gapped positions (so this similarity is asymmetric)
+            if pair_matches / L_non_gaps[i] >= identity_threshold:
+                num_neighbors_i += 1
+
+        num_neighbors[i] = num_neighbors_i
+
+    return num_neighbors
+
+
+@jit(nopython=True)
+def num_cluster_members_nogaps_serial(matrix, identity_threshold, invalid_value):
+    """
+    Calculate number of sequences in alignment
+    within given identity_threshold of each other
+
+    Parameters
+    ----------
+    matrix : np.array
+        N x L matrix containing N sequences of length L.
+        Matrix must be mapped to range(0, num_symbols) using
+        map_matrix function
+    identity_threshold : float
+        Sequences with at least this pairwise identity will be
+        grouped in the same cluster.
+    invalid_value : int
+            Value in matrix that is considered invalid, e.g. gap or lowercase character.
+    Returns
+    -------
+    np.array
+        Vector of length N containing number of cluster
+        members for each sequence (inverse of sequence
+        weight)
+    """
+    N, L = matrix.shape
+    L = 1.0 * L
+
+    # minimal cluster size is 1 (self)
+    num_neighbors = np.ones((N))
+    L_non_gaps = L - np.sum(matrix == invalid_value, axis=1)  # Edit: From EVE, use the non-gapped length
+
+    # compare all pairs of sequences
+    # Edit: Rewrote loop without any dependencies between inner and outer loops, so that it can be parallelized
+    for i in range(N):
+        num_neighbors_i = 1
         for j in range(N):
             if i == j:
                 continue
--- test/TestWeightsCalc.py
+++ test/TestWeightsCalc.py
@@ -1,75 +1,131 @@
 import unittest
 from unittest import TestCase
 
+import numba
 import numpy as np
 
-from evcouplings.align import num_cluster_members_legacy, num_cluster_members_parallel
+from evcouplings.align import num_cluster_members_legacy, num_cluster_members_nogaps_parallel, map_from_alphabet, map_matrix, \
+    ALPHABET_PROTEIN, MATCH_GAP, sequences_to_matrix
+
+# Alphabet : "-" + "ACDEFGHIKLMNPQRSTVWY"
+
+alphabet_map = map_from_alphabet(
+    ALPHABET_PROTEIN, default=MATCH_GAP,
+)
+invalid_value = 0  # GAP character is mapped to 0
+
+
+def sequences_to_mapped_matrix(sequences):
+    return map_matrix(sequences_to_matrix(sequences), alphabet_map)
 
 
 class TestWeights(TestCase):
 
-    def test_num_cluster_members_basic(self):
-        # Basic test
-        invalid_value = -1  # Usually the matrix has to be between (0, num_symbols) but here we can do what we want
+    def test_helper(self, sequences, expected, identity_threshold):
+        matrix = sequences_to_mapped_matrix(sequences)
+        num_neighbours = num_cluster_members_nogaps_parallel(matrix, identity_threshold=identity_threshold, invalid_value=invalid_value)
+        self.assertTrue(np.array_equal(num_neighbours, expected),
+                        "Expected: {}\nGot: {}".format(expected, num_neighbours))
 
-        # Symmetric and all equal
+    def test_mapping(self):
+        # Just a sanity check about the alphabet used
+        seqs = ["ACD-",
+                "ACD-"]
+        seq_matrix = sequences_to_matrix(seqs)
+        matrix_mapped = map_matrix(seq_matrix, alphabet_map)
         matrix = np.array(
-            [[0, 1, 2, -1],
-             [0, 1, 2, -1],
-             [0, 1, 2, -1]]
+            [[1, 2, 3, 0],
+             [1, 2, 3, 0]]
         )
+        self.assertTrue(np.array_equal(matrix_mapped, matrix), "Expected: {}\nGot: {}".format(matrix, matrix_mapped))
+
+    def test_nogaps_basic(self):
+        # Basic test
+        # Symmetric and all equal
+        seqs = ["ACD-",
+                "ACD-",
+                "ACD-"]
+        matrix = sequences_to_mapped_matrix(seqs)
         expected = np.array([3, 3, 3])
         # num_neighbours = num_cluster_members(matrix, identity_threshold=0.8)  #, invalid_value=invalid_value
-        num_neighbours = num_cluster_members_parallel(matrix, identity_threshold=0.8, invalid_value=invalid_value)
+        num_neighbours = num_cluster_members_nogaps_parallel(matrix, identity_threshold=0.8, invalid_value=invalid_value)
         self.assertTrue(np.array_equal(num_neighbours, expected), "Expected: {}\nGot: {}".format(expected, num_neighbours))
 
         # Just below 0.5 sequence similarity threshold
         # Gaps not counted, so 1/3 similarity < 0.5
-        matrix = np.array(
-            [[0, 1, 2, -1],
-             [0, 0, 0, -1]]
-        )
+        seqs = ["ACD-",
+                "AAA-"]
+        matrix = sequences_to_mapped_matrix(seqs)
         expected = np.array([1, 1])
-        num_neighbours = num_cluster_members_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
+        num_neighbours = num_cluster_members_nogaps_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
         self.assertTrue(np.array_equal(num_neighbours, expected),
                         "Expected: {}\nGot: {}".format(expected, num_neighbours))
 
         # Mostly gaps, so sequences are just [0] and [1]
-        matrix = np.array(
-            [[0, -1, -1, -1, -1],
-             [1, -1, -1, -1, -1]]
-        )
+        seqs = ["A----",
+                "C----"]
+        matrix = sequences_to_mapped_matrix(seqs)
         expected = np.array([1, 1])
-        num_neighbours = num_cluster_members_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
+        num_neighbours = num_cluster_members_nogaps_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
         self.assertTrue(np.array_equal(num_neighbours, expected),
                         "Expected: {}\nGot: {}".format(expected, num_neighbours))
 
         # Not counting its own gaps makes the threshold asymmetric
         # Sequence A: only [2,3] our of 5 overlap, so 0.4 < 0.5, so sequence on its own
         # Sequence B: Only [2,3] out of 3 non-gap overlap, so 0.667 > 0.5 and seq A is a neighbour of seq B
-        matrix = np.array(
-            [[0, 1, 2, 3, 0, 0],
-             [-1, -1, 2, 3, 4, -1]]
-        )
+        seqs = ["ACMMAA",
+                "--MMY-"]
+        matrix = sequences_to_mapped_matrix(seqs)
+        # matrix = np.array(
+        #     [[1, 2, 3, 4, 1, 1],
+        #      [0, 0, 3, 4, 5, 0]]
+        # )
         expected = np.array([1, 2])
-        num_neighbours = num_cluster_members_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
+        num_neighbours = num_cluster_members_nogaps_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
         self.assertTrue(np.array_equal(num_neighbours, expected),
                         "Expected: {}\nGot: {}".format(expected, num_neighbours))
 
     def test_num_cluster_fragment(self):
-        invalid_value = -1  # Usually the matrix has to be between (0, num_symbols) but here we can do what we want
-        # Problem: N sequences appear in cluster together (so num_cluster_members is N) but a fragment appears as its own cluster
-
-        matrix = np.array(
-            [[0, 1, 2, 3, 4, 5, 6],
-             [0, 1, 2, 3, 4, 5, 7],
-             [0, 1, 1, 3, 4, 5, 6],
-             [0, 1, 2, -1, -1, -1, -1],  # Fragment
-             ]
-        )
-        # Note: The fragment isn't a neighbour of the full sequences, but the full sequences are neighbours of the fragment
+        # Problem with fragments: N sequences appear in cluster together (so num_cluster_members is N) but a fragment appears as its own cluster
+        seqs = ["ACDMMMA",
+                "ACDMMMY",
+                "ACCMMMA",
+                "ACD----"]  # Fragment
+        # Note: The fragment isn't a neighbour of the full sequences (2 or 3 positions out of 7), but the full sequences are neighbours of the fragment
         expected = np.array([3, 3, 3, 4])
-        num_neighbours = num_cluster_members_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
+
+        matrix = sequences_to_mapped_matrix(seqs)
+        # matrix = np.array(
+        #     [[0, 1, 2, 3, 4, 5, 6],
+        #      [0, 1, 2, 3, 4, 5, 7],
+        #      [0, 1, 1, 3, 4, 5, 6],
+        #      [0, 1, 2, -1, -1, -1, -1],  # Fragment
+        #      ]
+        # )
+        num_neighbours = num_cluster_members_nogaps_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
+        self.assertTrue(np.array_equal(num_neighbours, expected),
+                        "Expected: {}\nGot: {}".format(expected, num_neighbours))
+
+    def test_many_gaps(self):
+        # Testing a non-fragment sequence with many gaps
+        seqs = ["A-C-DM-",
+                "A--YL-A"]
+        expected = np.array([1, 1])
+
+        matrix = sequences_to_mapped_matrix(seqs)
+        num_neighbours = num_cluster_members_nogaps_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
+        self.assertTrue(np.array_equal(num_neighbours, expected),
+                        "Expected: {}\nGot: {}".format(expected, num_neighbours))
+
+    def test_gaps_nomatch(self):
+        # If gaps were counted as matches, this would be a cluster with 2 sequences (5 out of 6 matches)
+        # Instead the overlap is 1/3 matches, so not above threshold
+        seqs = ["ACD---",
+                "AYY---"]
+        expected = np.array([1, 1])
+
+        matrix = sequences_to_mapped_matrix(seqs)
+        num_neighbours = num_cluster_members_nogaps_parallel(matrix, identity_threshold=0.5, invalid_value=invalid_value)
         self.assertTrue(np.array_equal(num_neighbours, expected),
                         "Expected: {}\nGot: {}".format(expected, num_neighbours))
 
