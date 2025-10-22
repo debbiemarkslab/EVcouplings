30e7903b920eae051debdf0330f76e3f120554f1
Thomas Hopf <thomas.hopf@gmail.com>
2025-10-22 10:49:53 +0200

    remove version from alphafolddb literal

--- evcouplings/compare/protocol.py
+++ evcouplings/compare/protocol.py
@@ -36,7 +36,7 @@ from evcouplings.compare.ecs import (
 from evcouplings.visualize import pairs, misc
 
 SIFTS_TABLE_FORMAT_STR = "{pdb_id}:{pdb_chain} ({coord_start}-{coord_end})"
-AVAILABLE_MODEL_DB_TYPES = ["alphafolddb_v4"]
+AVAILABLE_MODEL_DB_TYPES = ["alphafolddb"]
 ALPHAFOLDDB_DOWNLOAD_URL = "https://alphafold.ebi.ac.uk/files/{id}.cif"
 
 
@@ -664,7 +664,7 @@ def _identify_predicted_structures(**kwargs):
         )
 
     table_callback = None
-    if modeldb_type == "alphafolddb_v4":
+    if modeldb_type == "alphafolddb":
         table_callback = lambda ali, hits: (
             _map_alphafold_hits(
                 kwargs["modeldb_list_file"], set(hits.uniprot_ac)
@@ -732,7 +732,7 @@ def _load_models(model_ids, modeldb_type, structure_dir=None, raise_missing=True
         )
 
     # implement database-specific retrieval behaviour here
-    if modeldb_type == "alphafolddb_v4":
+    if modeldb_type == "alphafolddb":
         make_download_url = lambda model_id: ALPHAFOLDDB_DOWNLOAD_URL.format(id=model_id)
 
     structures = {}
